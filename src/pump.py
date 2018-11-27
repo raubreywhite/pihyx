91d6f7ab05551395f08edc90b95c713f50ba7029
Richard White <w@rwhite.no>
2018-11-27 19:06:09 +0200

    Update pump.py

--- src/pump.py
+++ src/pump.py
@@ -89,7 +89,7 @@ def main():
   device = AtlasI2C()   # creates the I2C port object, specify the address or bus if necessary
   
   device.set_i2c_address(103)
-  print(device.query("Find"))
+  print(device.query("R"))
 
 if __name__ == '__main__':
   main()
